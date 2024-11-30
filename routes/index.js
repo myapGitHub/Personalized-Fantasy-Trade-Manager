53247ced4bdedf5efbb769dd7773a1840a27d1be
myapGitHub <matthewyapg@gmail.com>
2024-11-30 18:49:40 -0500

    Updates route index.js
    Needed to export constructorMethod to use in other files

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
-import path from 'path';
-import signupRoutes from './sign-up.js';
-import signinRoutes from './login.js';
-import privateRoutes from './private.js';
-
+import path from "path";
+import signupRoutes from "./sign-up.js";
+import signinRoutes from "./login.js";
+import privateRoutes from "./private.js";
 
 export const constructorMethod = (app) => {
-    app.use('/sign-up', signupRoutes);
-    app.use('/login', signinRoutes);
-    app.use('/private', privateRoutes);
-    app.use('/logout', async (req, res) => {
-        req.session.destroy();
-        res.redirect('/');
-    })
-    app.use('/', (req,res) => {
-        res.render('pages/home', {loggedIn: req.isLoggedIn, user: req.user});
-        //res.render('pages/home', {loggedIn: true, user: req.user});
-    });
-    app.use('*', (req, res) => {
-        res.redirect('/');
-    });
+  app.use("/sign-up", signupRoutes);
+  app.use("/login", signinRoutes);
+  app.use("/private", privateRoutes);
+  app.use("/logout", async (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+  });
+  app.use("/", (req, res) => {
+    res.render("pages/home", { loggedIn: req.isLoggedIn, user: req.user });
+    //res.render('pages/home', {loggedIn: true, user: req.user});
+  });
+  app.use("*", (req, res) => {
+    res.redirect("/");
+  });
 };
 
-
+export default constructorMethod;
