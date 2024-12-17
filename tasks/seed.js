1faea5e44160776efb5dd9bb52726defe4fef21e
tyler <tspeedy@stevens.edu>
2024-12-16 21:42:14 -0500

    Seed

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,6 +1,18 @@
-import {dbConnection, closeConnection} from '../config/mongoConnection.js';
+import { dbConnection, closeConnection } from '../config/mongoConnection.js';
+import { workoutData } from "../data/index.js";
+import { userData } from "../data/index.js";
 
 const db = await dbConnection();
 await db.dropDatabase();
+
+const tyler = await userData.signUp('tspeedy', 'password', 'Tyler', 'Speedy', 50, 50, 50, 'male', 1, 1, 1, 1);
+const tid = tyler.userId;
+
+const jimmy = await userData.signUp('jmurphy', 'password', 'James', 'Murphy', 50, 50, 50, 'male', 1, 1, 1, 1);
+const jid = tyler.userId;
+
+const workoutOne = await workoutData.createWorkoutPlan('tspeedy', 'Workout', 'Bench', [{ name: 'Bench', sets: 1, reps: 1, weight: 121 }, { name: 'Squat', sets: 1, reps: 1, weight: 121 }], 3);
+const workoutTwo = await workoutData.createWorkoutPlan('tspeedy', 'Workout2', 'Squat', [{ name: 'Squat', sets: 1, reps: 1, weight: 121 }, { name: 'Deadlift', sets: 1, reps: 1, weight: 121 }], 2);
+const workoutThree = await workoutData.createWorkoutPlan('jmurphy', 'Workout3', 'Deadlift', [{ name: 'Deadlift', sets: 1, reps: 1, weight: 121 }],);
 
 await closeConnection();
