97b1bf05003425ed326b7b9d59836f3800c5bd66
tyler <tspeedy@stevens.edu>
2024-12-16 21:59:07 -0500

    Fixed seed

--- tasks/seed.js
+++ tasks/seed.js
@@ -5,14 +5,14 @@ import { userData } from "../data/index.js";
 const db = await dbConnection();
 await db.dropDatabase();
 
-const tyler = await userData.signUp('tspeedy', 'password', 'Tyler', 'Speedy', 50, 50, 50, 'male', 1, 1, 1, 1);
+const tyler = await userData.signUp('tspeedy', 'P@2ssssss', 'Tyler', 'Speedy', 61, 62, 63, 'male', 64, 65, 66, 'beginner');
 const tid = tyler.userId;
 
-const jimmy = await userData.signUp('jmurphy', 'password', 'James', 'Murphy', 50, 50, 50, 'male', 1, 1, 1, 1);
+const jimmy = await userData.signUp('jmurphy', 'P@2ssssss', 'James', 'Murphy', 67, 68, 69, 'male', 70, 71, 72, 'intermediate');
 const jid = tyler.userId;
 
 const workoutOne = await workoutData.createWorkoutPlan('tspeedy', 'Workout', 'Bench', [{ name: 'Bench', sets: 1, reps: 1, weight: 121 }, { name: 'Squat', sets: 1, reps: 1, weight: 121 }], 3);
 const workoutTwo = await workoutData.createWorkoutPlan('tspeedy', 'Workout2', 'Squat', [{ name: 'Squat', sets: 1, reps: 1, weight: 121 }, { name: 'Deadlift', sets: 1, reps: 1, weight: 121 }], 2);
-const workoutThree = await workoutData.createWorkoutPlan('jmurphy', 'Workout3', 'Deadlift', [{ name: 'Deadlift', sets: 1, reps: 1, weight: 121 }],);
+const workoutThree = await workoutData.createWorkoutPlan('jmurphy', 'Workout3', 'Deadlift', [{ name: 'Deadlift', sets: 1, reps: 1, weight: 121 }],1);
 
 await closeConnection();
